eca3a6335e8dedf5c743dd5f87302c171f220924
dingdekai <dekai.ding@shen021.com>
2018-12-21 11:36:40 +0800

    [Add] 171 paragraph.

---
+++ algorithms/171_Excel Sheet Column Number/main_math_pow_8ms.cpp
@@ -0,0 +1,38 @@
+﻿//Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+#include "iostream"
+#include "math.h"
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int titleToNumber(string s) {
+		int result = 0;
+		for (int i = s.size()-1; i >= 0; --i)
+		{
+			result += (s[i] - 'A' + 1) * pow(26, s.size() - 1-i);
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->titleToNumber("ZY");
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
---
+++ algorithms/171_Excel Sheet Column Number/main_reserve_value_4ms.cpp
@@ -0,0 +1,40 @@
+﻿//Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+#include "iostream"
+#include "math.h"
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int titleToNumber(string s) {
+		int result = 0;
+		int carry = 1;
+		for (int i = s.size()-1; i >= 0; --i)
+		{
+			result += (s[i] - 'A' + 1) * carry;
+			carry *= 26;
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->titleToNumber("ZY");
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
