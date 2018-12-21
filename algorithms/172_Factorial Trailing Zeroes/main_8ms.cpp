120214f28ac3d9f0521b4b5f8128bd1b1c027bc1
dingdekai <dekai.ding@shen021.com>
2018-12-21 12:22:01 +0800

    [Add] 172 paragraph.

---
+++ algorithms/172_Factorial Trailing Zeroes/main_8ms.cpp
@@ -0,0 +1,38 @@
+﻿//Given an integer n, return the number of trailing zeroes in n!.
+
+#include "iostream"
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int trailingZeroes(int n) {
+		int result = 0;
+		while (n)
+		{
+			result += n / 5;
+			n /= 5;
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->trailingZeroes(1808548329);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
---
+++ algorithms/172_Factorial Trailing Zeroes/main_recursive.cpp
@@ -0,0 +1,32 @@
+﻿//Given an integer n, return the number of trailing zeroes in n!.
+
+#include "iostream"
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int trailingZeroes(int n) {
+		return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->trailingZeroes(1808548329);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
