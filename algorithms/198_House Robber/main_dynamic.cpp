9a2b016f65111edce7f226bdfe3679a54f4a166c
dingdekai <dekai.ding@shen021.com>
2018-12-24 15:10:23 +0800

    [Add] 198 paragraph.

---
+++ algorithms/198_House Robber/main_dynamic.cpp
@@ -0,0 +1,41 @@
+﻿//Given an array, rotate the array to the right by k steps, where k is non - negative.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		if (nums.size() <= 1)
+			return nums.empty() ? 0 : nums[0];
+		vector<int> dp = { nums[0], max(nums[0], nums[1]) };
+		for (int i = 2; i < nums.size(); ++i)
+		{
+			dp.push_back(max(nums[i] + dp[i - 2], dp[i - 1]));
+		}
+		return dp.back();
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	vector<int> input = { 1, 2, 3, 1, 5, 20, 0 };
+	auto result = solution->rob(input);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
---
+++ algorithms/198_House Robber/main_recursive.cpp
@@ -0,0 +1,49 @@
+﻿//Given an array, rotate the array to the right by k steps, where k is non - negative.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		int n = nums.size();
+		if (n == 0)
+		{
+			return 0;
+		}
+		if (n == 1)
+		{
+			return nums[0];
+		}
+		if (n == 2)
+		{
+			return max(nums[0], nums[1]);
+		}
+		vector<int> v1 = vector<int>(nums.begin() + 1, nums.end());
+		vector<int> v2 = vector<int>(nums.begin() + 2, nums.end());
+		return max(rob(v1), nums[0] + rob(v2));
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	vector<int> input = { 1, 2, 3, 1, 5, 20, 17 };
+	auto result = solution->rob(input);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
