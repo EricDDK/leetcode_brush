bc8239d85b502328cbc437c8d7fc3d4d77c38adc
dingdekai <dekai.ding@shen021.com>
2018-12-24 17:07:03 +0800

    [Add] 204 paragraph.

---
+++ algorithms/204_Count Primes/main_vector_20ms.cpp
@@ -0,0 +1,47 @@
+﻿//Count the number of prime numbers less than a non - negative number, n.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int countPrimes(int n) {
+		int res = 0;
+		vector<bool> prime(n, true);
+		for (int i = 2; i < n; ++i)
+		{
+			if (prime[i])
+			{
+				res++;
+				for (int j = 2; i * j < n; ++j)
+				{
+					prime[i * j] = false;
+				}
+			}
+		}
+		return res;
+	}
+
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->countPrimes(499979);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
