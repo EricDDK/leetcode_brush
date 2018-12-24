24db0ed5bb62be11b35427d31f325f9a8c13221b
dingdekai <dekai.ding@shen021.com>
2018-12-24 16:48:24 +0800

    [Add] 203 paragraph.

---
+++ algorithms/203_Remove Linked List Elements/main_point_20ms.cpp
@@ -0,0 +1,72 @@
+﻿//Remove all elements from a linked list of integers that have value val.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* removeElements(ListNode* head, int val) {
+		ListNode* dummy = new ListNode(-1), *pre = dummy;
+		dummy->next = head;
+		while (pre->next)
+		{
+			if (pre->next->val == val)
+			{
+				ListNode* tmp = pre->next;
+				pre->next = pre->next->next;
+				delete tmp;
+				tmp = nullptr;
+			}
+			else
+			{
+				pre = pre->next;
+			}
+		}
+		return dummy->next;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	vector<int> v = { 1,1 };
+	ListNode* node = new ListNode(v[0]);
+	ListNode* head = node;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		node->next = new ListNode(v[i]);
+		node = node->next;
+	}
+	auto result = solution->removeElements(head, 1);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
+
+//vector<int> v = { 1, 2, 6, 3, 4, 5, 6 };
+//ListNode* node = new ListNode(v[0]);
+//ListNode* head = node;
+//for (int i = 1; i < v.size(); ++i)
+//{
+//	node->next = new ListNode(v[i]);
+//	node = node->next;
+//}
---
+++ algorithms/203_Remove Linked List Elements/main_recursive_20ms.cpp
@@ -0,0 +1,61 @@
+﻿//Remove all elements from a linked list of integers that have value val.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* removeElements(ListNode* head, int val) {
+		if (!head)
+		{
+			return NULL;
+		}
+		head->next = removeElements(head->next, val);
+		return head->val == val ? head->next : head;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	vector<int> v = { 1, 1 };
+	ListNode* node = new ListNode(v[0]);
+	ListNode* head = node;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		node->next = new ListNode(v[i]);
+		node = node->next;
+	}
+	auto result = solution->removeElements(head, 1);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
+
+//vector<int> v = { 1, 2, 6, 3, 4, 5, 6 };
+//ListNode* node = new ListNode(v[0]);
+//ListNode* head = node;
+//for (int i = 1; i < v.size(); ++i)
+//{
+//	node->next = new ListNode(v[i]);
+//	node = node->next;
+//}
