75d8e90e4f23e8c83d04de715a394ea9e140edde
dingdekai <dekai.ding@shen021.com>
2018-12-24 17:51:11 +0800

    [Add] 205 paragraph.

---
+++ algorithms/205_Isomorphic Strings/main_don't_know.cpp
@@ -0,0 +1,48 @@
+﻿/*Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the
+order of characters.No two characters may map to the same character but a character may map to itself.*/
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isIsomorphic(string s, string t) {
+		int cs[128] = { 0 }, ct[128] = { 0 };
+		for (int i = 0; i<s.size(); i++)
+		{
+			if (cs[s[i]] != ct[t[i]]) return false;
+			else if (!cs[s[i]]) //only record once
+			{
+				cs[s[i]] = i + 1;
+				ct[t[i]] = i + 1;
+			}
+		}
+		return true;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->isIsomorphic("egg", "add");
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
---
+++ algorithms/205_Isomorphic Strings/main_hash_16ms.cpp
@@ -0,0 +1,63 @@
+﻿/*Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the
+order of characters.No two characters may map to the same character but a character may map to itself.*/
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isIsomorphic(string s, string t) {
+		unordered_map<char, char> map;
+		unordered_map<char, char> reverse_map;
+		if (s.size() == t.size())
+		{
+			for (int i = 0; i < t.size(); ++i)
+			{
+				if (map.count(t[i]))
+				{
+					if (map[t[i]] != s[i])
+					{
+						return false;
+					}
+				}
+				if (map.count(t[i]) || reverse_map.count(s[i]))
+				{
+					if (map[t[i]] != s[i] || reverse_map[s[i]] != t[i])
+					{
+						return false;
+					}
+				}
+				map[t[i]] = s[i];
+				reverse_map[s[i]] = t[i];
+			}
+			return true;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	auto result = solution->isIsomorphic("egg", "add");
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
