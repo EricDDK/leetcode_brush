d41b7f03898cfd95ffc8370d993aae3fe02d5205
dingdekai <dekai.ding@shen021.com>
2018-12-26 20:26:35 +0800

    [Add] 217 paragraph.

---
+++ algorithms/217_Contains Duplicate/main_hash.cpp
@@ -0,0 +1,43 @@
+﻿//Reverse a singly linked list.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+		unordered_map<int, int> map;
+		for (auto &num : nums)
+		{
+			if (map.count(num))
+			{
+				return true;
+			}
+			map[num] = 0;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution s;
+	Solution* solution = new Solution;
+	vector<int> input = { 1, 2, 3, 1 };
+	auto result = solution->containsDuplicate(input);
+	cout << result << endl;
+	delete solution;
+	system("pause");
+	return 0;
+}
