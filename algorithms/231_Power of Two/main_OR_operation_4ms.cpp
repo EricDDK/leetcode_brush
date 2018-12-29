683eb8c5205e2e418b2a856a3cf5d30ed03e519f
dingdekai <dekai.ding@shen021.com>
2018-12-29 12:09:32 +0800

    [Add] 231 paragraph.

---
+++ algorithms/231_Power of Two/main_OR_operation_4ms.cpp
@@ -0,0 +1,43 @@
+﻿//Given an integer, write a function to determine if it is a power of two.
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+		if (n > 0 && ((n & (n - 1)) == 0))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPowerOfTwo(16);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
