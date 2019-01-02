7a7ce8233e24b5cf1e08b603bb602a61d60503d0
dingdekai <dekai.ding@shen021.com>
2019-01-02 16:26:44 +0800

    [Add] 242 paragraph.

---
+++ algorithms/242_Valid Anagram/main_hash_20ms.cpp
@@ -0,0 +1,60 @@
+﻿//Lowest Common Ancestor of a Binary Search Tree
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isAnagram(string s, string t) {
+		if (s.size() != t.size())
+		{
+			return false;
+		}
+		unordered_map<char, int> map;
+		for (auto &str : s)
+		{
+			if (map.count(str))
+			{
+				map[str]++;
+				continue;
+			}
+			map[str] = 1;
+		}
+		for (auto &str : t)
+		{
+			if (!map.count(str))
+			{
+				return false;
+			}
+			else
+			{
+				if (--(map[str]) < 0)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isAnagram("anagram","nagaram");
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/242_Valid Anagram/main_hash_array_8ms.cpp
@@ -0,0 +1,49 @@
+﻿//Lowest Common Ancestor of a Binary Search Tree
+
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isAnagram(string s, string t) {
+		if (s.size() != t.size())
+		{
+			return false;
+		}
+		int a[26] = { 0 };
+		for (int i = 0; i < s.size(); ++i)
+		{
+			a[s[i] - 'a']++;
+			a[t[i] - 'a']--;
+		}
+		for (int i = 0; i < 26; i++)
+		{
+			if (a[i] < 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isAnagram("anagram","nagaram");
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
