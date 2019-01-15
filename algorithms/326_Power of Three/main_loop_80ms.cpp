b1e5b177445293c8caf4372e4cd5fa20d954b915
dingdekai <dekai.ding@shen021.com>
2019-01-15 19:38:58 +0800

    [Add] 326 paragraph.

---
+++ algorithms/326_Power of Three/main_loop_80ms.cpp
@@ -0,0 +1,41 @@
+﻿//Given an integer array nums, find the sum of the elements between indices i and j(i ≤ j), inclusive.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfThree(int n) {
+		if (n < 1)
+		{
+			return false;
+		}
+		while (n % 3 == 0)
+		{
+			n /= 3;
+		}
+		return n== 1;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPowerOfThree(45);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/326_Power of Three/main_math_method_4ms.cpp
@@ -0,0 +1,33 @@
+﻿//Given an integer array nums, find the sum of the elements between indices i and j(i ≤ j), inclusive.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfThree(int n) {
+		return (n > 0 && 1162261467 % n == 0);
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPowerOfThree(45);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
