902fdbde779d9d7c44670f436168a029ae27ac90
dingdekai <dekai.ding@shen021.com>
2019-01-16 17:47:21 +0800

    [Add] 344 paragraph.

---
+++ algorithms/344_Reverse String/main_2_pointer_4ms.cpp
@@ -0,0 +1,41 @@
+﻿//Given an integer(signed 32 bits), write a function to check whether it is a power of 4.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	string reverseString(string s) {
+		int left = 0;
+		int right = s.size() - 1;
+		while (left < right)
+		{
+			char tmp = s[right];
+			s[right--] = s[left];
+			s[left++] = tmp;
+		}
+		return s;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->reverseString("hello");
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
