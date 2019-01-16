63c2011856ac69e175e1fb4a9bcb0f5f5aecf4cb
dingdekai <dekai.ding@shen021.com>
2019-01-16 17:43:56 +0800

    [Add] 342 paragraph.

---
+++ algorithms/342_Power of Four/main_4ms.cpp
@@ -0,0 +1,33 @@
+﻿//Given an integer(signed 32 bits), write a function to check whether it is a power of 4.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfFour(int num) {
+		return num > 0 && !(num & (num - 1)) && (num - 1) % 3 == 0;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPowerOfFour(2);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/342_Power of Four/main_loop_8ms.cpp
@@ -0,0 +1,37 @@
+﻿//Given an integer(signed 32 bits), write a function to check whether it is a power of 4.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfFour(int num) {
+		while (num && (num % 4 == 0)) 
+		{
+			num /= 4;
+		}
+		return num == 1;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPowerOfFour(2);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
