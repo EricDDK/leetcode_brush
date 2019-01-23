d367ec33a01b355abdb280333ec47db7357d8cae
dingdekai <dekai.ding@shen021.com>
2019-01-23 19:51:44 +0800

    [Add] 371 paragraph.

---
+++ algorithms/371_Sum of Two Integers/main_bit_operation_recursive_0ms.cpp
@@ -0,0 +1,39 @@
+﻿//Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int getSum(int a, int b) {
+		if (b == 0)
+		{
+			return a;
+		}
+		int sum = a ^ b;
+		int carry = (a & b) << 1;
+		return getSum(sum, carry);
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->getSum(1, 2);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
