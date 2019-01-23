79c79b561eb7acbfd5ccc86cb9d4ae2c81628c9c
dingdekai <dekai.ding@shen021.com>
2019-01-23 19:19:56 +0800

    [Add] 367 paragraph.

---
+++ algorithms/367_Valid Perfect Square/main_O(n)_0ms.cpp
@@ -0,0 +1,40 @@
+﻿//Given two arrays, write a function to compute their intersection.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPerfectSquare(int num) 
+	{
+		if (num == 1) 
+			return true;
+		int min = num / 2, max = num/2;
+		while (min * min > num)
+		{
+
+		}
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPerfectSquare(14);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/367_Valid Perfect Square/main_binary_search_0ms.cpp
@@ -0,0 +1,49 @@
+﻿//Given two arrays, write a function to compute their intersection.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPerfectSquare(int num) 
+	{
+		int high = 46340, low = 0, mid;
+		while (low <= high) {
+
+			mid = low + (high - low) / 2;
+
+			if (mid*mid > num) {
+				high = mid - 1;
+			}
+			else if (mid*mid < num) {
+				low = mid + 1;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->isPerfectSquare(14);
+	cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
