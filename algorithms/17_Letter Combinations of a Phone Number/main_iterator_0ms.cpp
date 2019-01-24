e638882c57e00a39d6a0e367a57dc69c5e508f79
dingdekai <dekai.ding@shen021.com>
2019-01-24 13:16:02 +0800

    [Add] 17 paragraph.

---
+++ algorithms/17_Letter Combinations of a Phone Number/main_iterator_0ms.cpp
@@ -0,0 +1,52 @@
+﻿//Given a string containing digits from 2 - 9 inclusive, return all possible letter combinations that the number could represent.
+//
+//A mapping of digit to letters(just like on the telephone buttons) is given below.Note that 1 does not map to any letters.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> letterCombinations(string digits) {
+		if (digits.empty())
+			return{};
+		vector<string> res{ "" };
+		string dict[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+		for (int i = 0; i < digits.size(); ++i)
+		{
+			vector<string> tmp;
+			string str = dict[digits[i] - '0'];
+			for (int j = 0; j < str.size(); j++)
+			{
+				for (auto s : res)
+				{
+					tmp.push_back(s + str[j]);
+				}
+			}
+			res = tmp;
+		}
+		return res;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->letterCombinations("2345");
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/17_Letter Combinations of a Phone Number/main_recursive_0ms.cpp
@@ -0,0 +1,54 @@
+﻿//Given a string containing digits from 2 - 9 inclusive, return all possible letter combinations that the number could represent.
+//
+//A mapping of digit to letters(just like on the telephone buttons) is given below.Note that 1 does not map to any letters.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> letterCombinations(string digits) {
+		if (digits.empty()) 
+			return {};
+		vector<string> result;
+		string dict[10] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+		operateLetters(result, digits, 0, "", dict);
+		return result;
+	}
+
+	void operateLetters(vector<string> &result, string digits, int index, string res, string dict[])
+	{
+		if (index == digits.size())
+		{
+			result.push_back(res);
+			return;
+		}
+		string str = dict[digits[index] - '0'];
+		for (int i = 0; i < str.size(); ++i)
+		{
+			operateLetters(result, digits, index + 1, res + string(1, str[i]), dict);
+		}
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->letterCombinations("23");
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
