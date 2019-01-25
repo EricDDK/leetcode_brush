37f4d9708f3b8403fa1384242a6f259e3212587a
dingdekai <dekai.ding@shen021.com>
2019-01-25 15:22:31 +0800

    [Add] 18 paragraph.

---
+++ algorithms/18_4Sum/main_4_loop_392ms.cpp
@@ -0,0 +1,57 @@
+﻿//Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums 
+//such that a + b + c + d = target ? Find all unique quadruplets in the array which gives the sum of target.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		set<vector<int>> result;
+		sort(nums.begin(), nums.end());
+		//if (nums.empty() || nums.back() > target || nums.front() < target || nums.size() < 4)
+		//{
+		//	return {};
+		//}
+		for (int i = 0; (i < nums.size()); ++i)
+		{
+			for (int j = i + 1; (j < nums.size()); ++j)
+			{
+				for (int k = j + 1; (k < nums.size()); ++k)
+				{
+					for (int l = k + 1; (l < nums.size()); ++l)
+					{
+						if (nums[i] + nums[j] + nums[k] + nums[l] == target )
+						{
+							result.insert({ nums[i], nums[j], nums[k], nums[l] });
+						}
+					}
+				}
+			}
+		}
+		return vector<vector<int>>(result.begin(), result.end());
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> input = { 1, -2, -5, -4, -3, 3, 3, 5 };
+	auto result = solution->fourSum(input, 11);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/18_4Sum/main_O(n3)_20ms.cpp
@@ -0,0 +1,53 @@
+﻿//Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums 
+//such that a + b + c + d = target ? Find all unique quadruplets in the array which gives the sum of target.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		set<vector<int>> res;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < int(nums.size() - 3); ++i) {
+			for (int j = i + 1; j < int(nums.size() - 2); ++j) {
+				if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+				int left = j + 1, right = nums.size() - 1;
+				while (left < right) {
+					int sum = nums[i] + nums[j] + nums[left] + nums[right];
+					if (sum == target) {
+						vector<int> out{ nums[i], nums[j], nums[left], nums[right] };
+						res.insert(out);
+						++left; --right;
+					}
+					else if (sum < target) ++left;
+					else --right;
+				}
+			}
+		}
+		return vector<vector<int>>(res.begin(), res.end());
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> input = { 1, -2, -5, -4, -3, 3, 3, 5 };
+	auto result = solution->fourSum(input, 11);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
