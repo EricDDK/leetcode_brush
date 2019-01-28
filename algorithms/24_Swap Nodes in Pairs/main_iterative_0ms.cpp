3fa426aaa6598e9c2d10c7102c06d5ba200d1895
dingdekai <dekai.ding@shen021.com>
2019-01-28 15:34:21 +0800

    [Add] 24 paragraph.

---
+++ algorithms/24_Swap Nodes in Pairs/main_iterative_0ms.cpp
@@ -0,0 +1,62 @@
+﻿//Given a linked list, swap every two adjacent nodes and return its head.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* swapPairs(ListNode* head) {
+		ListNode* dummy = new ListNode(-1);
+		ListNode* pre = dummy;
+		dummy->next = head;
+
+		while (pre->next && pre->next->next)
+		{
+			ListNode* tmp = pre->next->next;
+			pre->next->next = tmp->next;
+			tmp->next = pre->next;
+			pre->next = tmp;
+			pre = pre->next->next;
+		}
+
+		ListNode* result = dummy->next;
+		delete dummy;
+		return result;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> v = { 1, 2, 3, 4 };
+	ListNode* node = new ListNode(v[0]);
+	ListNode* input = node;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		node->next = new ListNode(v[i]);
+		node = node->next;
+	}
+	auto result = solution->swapPairs(input);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/24_Swap Nodes in Pairs/main_recursive_0ms.cpp
@@ -0,0 +1,54 @@
+﻿//Given a linked list, swap every two adjacent nodes and return its head.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* swapPairs(ListNode* head) {
+		if (!head || !head->next)
+		{
+			return head;
+		}
+		ListNode* tmp = head->next;
+		head->next = swapPairs(tmp->next);
+		tmp->next = head;
+		return tmp;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> v = { 1, 2, 3, 4 };
+	ListNode* node = new ListNode(v[0]);
+	ListNode* input = node;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		node->next = new ListNode(v[i]);
+		node = node->next;
+	}
+	auto result = solution->swapPairs(input);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
