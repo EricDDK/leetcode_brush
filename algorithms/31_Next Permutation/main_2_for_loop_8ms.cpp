477ec38d43f8c528407d8a4b8a0bede2b1abfbaf
dingdekai <dekai.ding@shen021.com>
2019-01-30 15:46:05 +0800

    [Add] 31 paragraph.

---
+++ algorithms/31_Next Permutation/main_2_for_loop_8ms.cpp
@@ -0,0 +1,62 @@
+﻿//Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+//
+//Return the quotient after dividing dividend by divisor.
+//
+//The integer division should truncate toward zero.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	void nextPermutation(vector<int>& nums) {
+		int size = nums.size();
+		int mid = size - 2;
+		for (int i = 1; i < size; ++i)
+		{
+			if (nums[i] < nums[i - 1])
+			{
+				mid = i - 2;
+				break;
+			}
+		}
+		for (int i = size - 1; i >= 0; --i)
+		{
+			if (nums[i] > nums[mid])
+			{
+				swap(nums[mid], nums[i]);
+				reverse(nums.begin() + mid + 1, nums.end());
+				break;
+			}
+		}
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> input = { 1,2 };
+	/*auto result = */solution->nextPermutation(input);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
---
+++ algorithms/31_Next Permutation/main_copy_4ms.cpp
@@ -0,0 +1,52 @@
+﻿//Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+//
+//Return the quotient after dividing dividend by divisor.
+//
+//The integer division should truncate toward zero.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	void nextPermutation(vector<int> &num) {
+		int i, j, n = num.size();
+		for (i = n - 2; i >= 0; --i) 
+		{
+			if (num[i + 1] > num[i]) 
+			{
+				for (j = n - 1; j > i; --j) 
+				{
+					if (num[j] > num[i]) break;
+				}
+				swap(num[i], num[j]);
+				reverse(num.begin() + i + 1, num.end());
+				return;
+			}
+		}
+		reverse(num.begin(), num.end());
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> input = { 1,2 };
+	/*auto result = */solution->nextPermutation(input);
+	//cout << result << endl;
+	delete(solution);
+	system("pause");
+	return 0;
+}
