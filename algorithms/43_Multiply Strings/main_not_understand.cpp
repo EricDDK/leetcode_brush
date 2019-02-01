7b9094a476e2c9e588a88e7d0619426bdf600cf5
dingdekai <dekai.ding@shen021.com>
2019-02-02 00:46:49 +0800

    [Add] 43 paragraph.

---
+++ algorithms/43_Multiply Strings/main_not_understand.cpp
@@ -0,0 +1,49 @@
+//Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+#include<sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+		string res;
+        int n1 = num1.size(), n2 = num2.size();
+        int k = n1 + n2 - 2, carry = 0;
+        vector<int> v(n1 + n2, 0);
+        for (int i = 0; i < n1; ++i) {
+            for (int j = 0; j < n2; ++j) {
+                v[k - i - j] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        for (int i = 0; i < n1 + n2; ++i) {
+            v[i] += carry;
+            carry = v[i] / 10;
+            v[i] %= 10;
+        }
+        int i = n1 + n2 - 1;
+        while (v[i] == 0) --i;
+        if (i < 0) return "0";
+        while (i >= 0) res.push_back(v[i--] + '0');
+        return res;
+    }
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->multiply("2", "3");
+	delete solution;
+	//cout << result << endl;
+	return 0;
+}
