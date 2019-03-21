6918a36a531d61ca8e5a87a39a7bee2a0be135ac
dingdekai <dekai.ding@shen021.com>
2019-03-21 15:36:09 +0800

    [Add] 82 paragraph.

---
+++ algorithms/82_Remove Duplicates from Sorted List II/main_recursive_8ms.cpp
@@ -0,0 +1,58 @@
+//Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+#include <sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include "windows.h"
+#include "algorithm"
+#include <stack>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (!head)
+			return head;
+		if (head->next && head->val == head->next->val)
+		{
+			while (head->next && head->val == head->next->val)
+			{
+				head = head->next;
+			}
+			return deleteDuplicates(head->next);
+		}
+		head->next = deleteDuplicates(head->next);
+		return head;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	vector<int> v = {1,2,3,3,4,4,5};
+	ListNode* node = new ListNode(v[0]);
+	ListNode* input = node;
+	for (int i = 1; i < v.size(); ++i)
+	{
+		node->next = new ListNode(v[i]);
+		node = node->next;
+	}
+	auto result = solution->deleteDuplicates(input);
+	//cout << result << endl
+	delete(solution);
+	system("pause");
+	return 0;
+}
