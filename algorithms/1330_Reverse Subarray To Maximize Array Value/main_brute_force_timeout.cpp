da1ce37a27c5749499ff9ee289401069a5f9cc8f
dingdekai <dekai.ding@shen021.com>
2020-02-20 16:01:53 +0800

    [Add] 1330 paragraph.

---
+++ algorithms/1330_Reverse Subarray To Maximize Array Value/main_brute_force_timeout.cpp
@@ -0,0 +1,74 @@
+﻿// You are given an integer array nums.The value of this array is defined as the sum of | nums[i] - nums[i + 1] | for all 0 <= i < nums.length - 1.
+//
+//	You are allowed to select any subarray of the given array and reverse it.You can perform this operation only once.
+//
+//	Find maximum possible value of the final array.
+
+#include <sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include "windows.h"
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <queue>
+#include <numeric>
+
+#ifdef _MSC_VER
+#include <crtdbg.h>
+#endif // _MSC_VER
+
+using namespace std;
+
+class Solution {
+public:
+	int maxValueAfterReverse(vector<int>& nums) {
+		int n = nums.size();
+		int left = 0, right = n - 1;
+		int sum = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = i; j < n; ++j)
+			{
+				int l = (i == 0 ? 0 : abs(nums[i] - nums[i - 1]));
+				int r = (j == n - 1 ? 0 : abs(nums[j] - nums[j + 1]));
+				int sum1 = l + r;
+				l = (i == 0 ? 0 : abs(nums[j] - nums[i - 1]));
+				r = (j == n - 1 ? 0 : abs(nums[i] - nums[j + 1]));
+				int sum2 = l + r;
+				if (sum2 - sum1 >= sum)
+				{
+					sum = sum2 - sum1;
+					left = i; right = j;
+				}
+			}
+		}
+		reverse(nums.begin() + left, nums.begin() + right + 1);
+		int result = 0;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			result += abs(nums[i] - nums[i + 1]);
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->maxValueAfterReverse(vector<int>{ 2, 3, 1, 5, 4 });
+	if (result != 10)
+		throw;
+	result = solution->maxValueAfterReverse(vector<int>{2, 4, 9, 24, 2, 1, 10});
+	if (result != 68)
+		throw;
+	delete solution;
+	system("pause");
+	return 0;
+}
---
+++ algorithms/1330_Reverse Subarray To Maximize Array Value/main_loop_O(n)_44ms.cpp
@@ -0,0 +1,72 @@
+﻿// You are given an integer array nums.The value of this array is defined as the sum of | nums[i] - nums[i + 1] | for all 0 <= i < nums.length - 1.
+//
+//	You are allowed to select any subarray of the given array and reverse it.You can perform this operation only once.
+//
+//	Find maximum possible value of the final array.
+
+#include <sstream>
+#include "iostream"
+#include <cstdint>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include "windows.h"
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <queue>
+#include <numeric>
+
+#ifdef _MSC_VER
+#include <crtdbg.h>
+#endif // _MSC_VER
+
+using namespace std;
+
+class Solution {
+public:
+	int maxValueAfterReverse(vector<int>& num) {
+		int n = num.size();
+		if (n == 1)
+			return num[0];
+		int sum = 0;
+		for (int i = 1; i < n; i++)
+			sum += abs(num[i] - num[i - 1]);
+		int mx = 0;
+		for (int i = 1; i < n - 1; i++)
+		{
+			mx = max(mx, abs(num[i + 1] - num[0]) - abs(num[i + 1] - num[i]));//单独考虑[1,i]翻转
+			mx = max(mx, abs(num[n - 1] - num[i - 1]) - abs(num[i] - num[i - 1]));//单独考虑[i,n]区间翻转
+		}
+		int dp[4];
+		for (int i = 0; i < 4; i++)
+			dp[i] = -1e9;
+		for (int i = 1; i < n; i++)
+		{
+			int x = abs(num[i] - num[i - 1]);
+			dp[0] = max(dp[0], num[i] - num[i - 1] - x);
+			dp[1] = max(dp[1], -num[i] - num[i - 1] - x);
+			dp[2] = max(dp[2], num[i] + num[i - 1] - x);
+			dp[3] = max(dp[3], -num[i] + num[i - 1] - x);
+		}
+		mx = max(mx, max(dp[0] + dp[3], dp[1] + dp[2]));
+		return sum + mx;
+	}
+};
+
+int main()
+{
+	Solution* solution = new Solution;
+	auto result = solution->maxValueAfterReverse(vector<int>{ 2, 3, 1, 5, 4 });
+	if (result != 10)
+		throw;
+	result = solution->maxValueAfterReverse(vector<int>{2, 4, 9, 24, 2, 1, 10});
+	if (result != 68)
+		throw;
+	delete solution;
+	system("pause");
+	return 0;
+}
